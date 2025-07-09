690dcf318979a4f9b58626f35b7ffed1b63740df
anju <anjuv1455@gmail.com>
2025-07-09 19:55:42 +0530

    initial commit

---
+++ src/main/java/com/example/azurejavaapp/AzurejavaappApplication.java
@@ -0,0 +1,13 @@
+package com.example.azurejavaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurejavaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzurejavaappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/azurejavaapp/AzurejavaappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azurejavaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzurejavaappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
