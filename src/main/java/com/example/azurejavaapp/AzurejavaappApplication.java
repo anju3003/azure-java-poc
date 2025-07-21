b532238126be764f08e5b0c95298b6d524dfedd2
anju <anjuv1455@gmail.com>
2025-07-21 12:16:40 +0530

    Added hardcoded password for SonarQube static analysis

--- src/main/java/com/example/azurejavaapp/AzurejavaappApplication.java
+++ src/main/java/com/example/azurejavaapp/AzurejavaappApplication.java
@@ -8,6 +8,10 @@ public class AzurejavaappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzurejavaappApplication.class, args);
+		String username = "admin";
+		String password = "P@ssw0rd123"; // Sensitive hardcoded password (SonarQube will flag this)
+		System.out.println("Logging in with username: " + username);
+
 	}
 
 }
